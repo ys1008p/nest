7c696c412183c65ca451378e3db34d078e3310f5
yspark <ys1008p@gmail.com>
2024-04-11 22:58:11 +0900

    테스트 임시 DB 구현(유효성 검사 추가 전)

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,47 @@
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  // :id와 search의 순서가 바뀌면 search를 id로 판단하여 search가 정상적으로 작동하지 않는다.
+  // @Get('search')
+  // search(@Query() query) {
+  //   return query;
+  // }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData: any) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
