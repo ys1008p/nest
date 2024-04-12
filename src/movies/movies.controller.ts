d22a859ad1670efaacbd19c66276bd59cc1809e8
yspark <ys1008p@gmail.com>
2024-04-12 09:33:44 +0900

    영화 생성 DTO 및 유효성 검증 추가
    - dto 폴더에 영화생성에 필요한 create dto 추가
    - 유효성 검증에 필요한 200~class-validator와 class-transformer 설치
    - path에 전달받는 id param을 transform 이용하여 string >> number로 자동
      형변환

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,3 +1,4 @@
+import { CreateMovieDto } from 'src/dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 import {
@@ -26,22 +27,23 @@ export class MoviesController {
   // }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string): Movie {
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
     return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
   @Delete(':id')
-  remove(@Param('id') movieId: string) {
+  remove(@Param('id') movieId: number) {
     return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: string, @Body() updateData: any) {
+  patch(@Param('id') movieId: number, @Body() updateData: any) {
     return this.moviesService.update(movieId, updateData);
   }
 }
