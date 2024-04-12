82f351804617a8d4dbe3d3cf011675e3508b9bef
yspark <ys1008p@gmail.com>
2024-04-12 16:48:10 +0900

    movies schema 파일 생성

---
+++ src/movies/schema/movies.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Movie extends Document {
+  @Prop({ required: true })
+  id: number;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  year: number;
+
+  @Prop({ type: [String], default: [] })
+  genres: string[];
+}
+
+export const MovieSchema = SchemaFactory.createForClass(Movie);
