0ae01e39950343b2b780c651bbbe2f20e5093f08
yspark <ys1008p@gmail.com>
2024-04-12 16:28:47 +0900

    환경변수 사용을 위한 config 설치 및 mongoDB 연결
    - npm i --save @nestjs/config 설치
    - app module에 mongoDB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    MoviesModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [],
 })
