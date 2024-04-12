65fd602853e9a285ef9142f458f0ccc1b722422a
yspark <ys1008p@gmail.com>
2024-04-12 10:43:04 +0900

    app과 module 분리 및 의존성 주입 설명 추가

---
+++ src/movies/movies.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MoviesController } from './movies.controller';
+import { MoviesService } from './movies.service';
+
+@Module({
+  controllers: [MoviesController],
+  //providers가 MoviesService의 모든 것을 import하여 controller에게 전달,
+  //타입을 추가하였을 뿐이지만 작동가능하게 해줌
+  //dependency injection이라고 부름
+  providers: [MoviesService],
+})
+export class MoviesModule {}
